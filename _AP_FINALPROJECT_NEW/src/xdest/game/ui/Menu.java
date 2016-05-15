fd13650615e617e203cd460833c65efab08f1b18
Zachary Carr <destroyer7712@gmail.com>
2016-05-15 13:19:35 -0400

    Added draw wave!
    It looks really nice

--- _AP_FINALPROJECT_NEW/src/xdest/game/ui/Menu.java
+++ _AP_FINALPROJECT_NEW/src/xdest/game/ui/Menu.java
@@ -17,34 +17,78 @@ public class Menu extends MouseAdapter implements Renderable {
 	private String name;
 	private UIButton[] buttons;
 	private BufferedImage bg;
-	
-	public Menu(String name, UIButton[] buttons, BufferedImage i)
-	{
+	private int x;
+
+	public Menu(String name, UIButton[] buttons, BufferedImage i) {
 		this.name = name;
 		this.buttons = buttons;
 		this.bg = i;
+		x = 0;
 	}
-	
-	public void render(Graphics g)
-	{
+
+	public void render(Graphics g) {
 		drawBg(g);
 		drawTitle(g);
+		drawWave(g);
 		drawOptions(g);
 	}
-	
-	public void addFloatingButton(UIButton b)
-	{
-		UIButton[] addedButton = new UIButton[buttons.length+1];
-		for (int i = 0; i < buttons.length; i++)
-		{
+
+	private void drawWave(Graphics g) {
+		Color last = g.getColor();
+		g.setColor(Color.cyan);
+		int rangeX = 600;
+		int rangeY = 600;
+		int startX = 0;
+		int startY = 0;
+		for (UIButton b : buttons) {
+			if (b.isHovered()) {
+				rangeX = (int) b.getBounds().getWidth() + 50;
+				rangeY = (int) b.getBounds().getHeight() + 50;
+				startX = (int) b.getBounds().getX() - 25;
+				startY = (int) b.getBounds().getY() - 25;
+				for (int i = 0; i < rangeX; i++) {
+					g.setColor(Color.blue);
+					g.fillRect(0 + startX + i, startY + (int) (Math.cos(Math.toRadians(x + i*2)) * 5), 1, 1);
+
+					g.setColor(Color.red);
+					g.fillRect(599 - i - startX, startY + rangeY + (int) (Math.cos(Math.toRadians(x + i*2)) * 5), 1, 1);
+				}
+				for (int i = 0; i < rangeY; i++) {
+					g.setColor(Color.blue);
+					g.fillRect(600 - startX + (int) (Math.sin(Math.toRadians(x + i*2)) * 5), startY + i, 1, 1);
+					g.setColor(Color.red);
+					g.fillRect(startX + (int) (Math.sin(Math.toRadians(x + i*2)) * 5), startY + (rangeY - i), 1, 1);
+				}
+				x++;
+				return;
+			}
+		}
+		for (int i = 0; i < rangeX; i++) {
+			// filled - g.drawLine(i, 300, i, 300 +
+			// (int)(Math.cos(Math.toRadians(x + (i*2))) * 20));
+			// unfilled
+			g.setColor(Color.blue);
+			g.fillRect(0 + i, 100 + (int) (Math.cos(Math.toRadians(x + i)) * 20), 1, 1);
+			g.fillRect(500+(int)(Math.cos(Math.toRadians(x + i)) * 20), i, 1, 1);
+			g.setColor(Color.red);
+			g.fillRect(599 - i, 500 + (int) (Math.cos(Math.toRadians(x + i)) * 20), 1, 1);
+			g.fillRect(100+(int)(Math.cos(Math.toRadians(x + i)) * 20), 599-i, 1, 1);
+		}
+		
+		g.setColor(last);
+		x++;
+	}
+
+	public void addFloatingButton(UIButton b) {
+		UIButton[] addedButton = new UIButton[buttons.length + 1];
+		for (int i = 0; i < buttons.length; i++) {
 			addedButton[i] = buttons[i];
 		}
-		addedButton[addedButton.length-1] = b;
+		addedButton[addedButton.length - 1] = b;
 		buttons = addedButton;
 	}
-	
-	private void drawBg(Graphics g)
-	{
+
+	private void drawBg(Graphics g) {
 		g.setColor(Color.PINK);
 		if (bg != null)
 			g.drawImage(bg, 0, 0, null);
@@ -52,54 +96,44 @@ public class Menu extends MouseAdapter implements Renderable {
 			g.fillRect(0, 0, Screen.WIDTH, Screen.HEIGHT);
 	}
 
-	private void drawOptions(Graphics g)
-	{
-		for (int i = 0; i < buttons.length; i++)
-		{
+	private void drawOptions(Graphics g) {
+		for (int i = 0; i < buttons.length; i++) {
 			buttons[i].render(g);
 		}
 	}
-	
-	private void drawTitle(Graphics g)
-	{
-		Graphics2D g2 = (Graphics2D)g;
+
+	private void drawTitle(Graphics g) {
+		Graphics2D g2 = (Graphics2D) g;
 		g2.setColor(Color.WHITE);
-		g2.setFont(new Font("Arial",0,30));
-		TextLayout tl = new TextLayout(name, new Font("Arial",0,30), ((Graphics2D)g).getFontRenderContext());
+		g2.setFont(new Font("Arial", 0, 30));
+		TextLayout tl = new TextLayout(name, new Font("Arial", 0, 30), ((Graphics2D) g).getFontRenderContext());
 		g2.drawRect((Screen.WIDTH / 12), 2, 10 * Screen.WIDTH / 12, 30);
-		g2.drawString(name, (int)((Screen.WIDTH / 2) - (tl.getBounds().getWidth() / 2)), 3 + 24);
+		g2.drawString(name, (int) ((Screen.WIDTH / 2) - (tl.getBounds().getWidth() / 2)), 3 + 24);
 	}
 
 	@Override
-	public void mouseClicked(MouseEvent e)
-	{
-		for (UIButton b : buttons)
-		{
-			if (b.getBounds().contains(e.getPoint()))
-			{
+	public void mouseClicked(MouseEvent e) {
+		for (UIButton b : buttons) {
+			if (b.getBounds().contains(e.getPoint())) {
 				b.clicked(Game.getCurrentGame());
 			}
 		}
 	}
-	
+
 	@Override
-	public void mouseMoved(MouseEvent e)
-	{
-		for (UIButton b : buttons)
-		{
-			if (b.getBounds().contains(e.getPoint()))
-			{
+	public void mouseMoved(MouseEvent e) {
+		for (UIButton b : buttons) {
+			if (b.getBounds().contains(e.getPoint())) {
 				b.setHover(true);
-			} else if (b.isHovered())
-			{
+			} else if (b.isHovered()) {
 				b.setHover(false);
 			}
 		}
 	}
-	
+
 	@Override
 	public boolean expired() {
 		return false;
 	}
-	
+
 }
