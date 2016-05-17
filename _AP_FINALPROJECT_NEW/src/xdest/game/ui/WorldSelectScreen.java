4009845d633508b0ca29b2f1965e25aac63f216f
Zachary Carr <destroyer7712@gmail.com>
2016-05-16 21:36:13 -0400

    World Select Screen

---
+++ _AP_FINALPROJECT_NEW/src/xdest/game/ui/WorldSelectScreen.java
@@ -0,0 +1,18 @@
+package xdest.game.ui;
+
+import javax.swing.JFrame;
+
+public class WorldSelectScreen extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5879307322118290784L;
+
+	public WorldSelectScreen() {
+		/*
+		 * This class will be to list worlds and select from one to play in! Will probably be called in GameAction after player names are asked for
+		 */
+	}
+
+}
